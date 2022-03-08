709e133102ede397005498a1f3eea800216bdafc
Timothée RIO <timothee.rio@student.ecp.fr>
2022-03-08 22:24:32 +0100

    working version

--- src/link_pred/grid_search.py
+++ src/link_pred/grid_search.py
@@ -11,27 +11,35 @@ def get_best_xgb(train_samples_scaled, train_labels, validation_samples_scaled, 
     best = 0.0
     best_model = None
     best_thresh = None
-    print(f"{'N_Estimators':^7} | {'Max_Depth':^7} | {'Min Child Weights':^7} | {'Learning_Rate':^7} | {'Accuracy':^12} | {'F1':^9} ")
+    print(f"{'N_Estimators':^7} | {'Max_Depth':^7} | {'Min Child Weights':^7} | {'Learning_Rate':^7} | {'Thresh':^12} | {'Accuracy':^12} | {'F1':^9} ")
 
-    for max_depth in tqdm(max_depth_candidates):
+    for max_depth in max_depth_candidates:
         for lr in learning_rates:
             for min_child_weight in min_child_weights:
-                clf = XGBClassifier(max_depth=max_depth, learning_rate=lr, min_child_weight=min_child_weight, n_estimators=n_estim, n_jobs=4, tree_method='gpu_hist', predictor="gpu_predictor", random_state=42, seed=42)
-                clf.fit(train_samples_scaled, train_labels, eval_metric="auc", early_stopping_rounds=300, eval_set=[(validation_samples_scaled, validation_labels)], verbose=0, use_label_encoder=False )
-                y_pred = clf.predict(validation_samples_scaled)
-                acc = accuracy_score(validation_labels, y_pred)
-                f1 = f1_score(validation_labels, y_pred)
+                clf = XGBClassifier(max_depth=max_depth, learning_rate=lr, min_child_weight=min_child_weight, n_estimators=n_estim, n_jobs=-1, tree_method='gpu_hist', predictor="gpu_predictor", random_state=42, seed=42)
+                clf.fit(train_samples_scaled, train_labels, eval_metric="auc", early_stopping_rounds=10, eval_set=[(validation_samples_scaled, validation_labels)], verbose=0)
+                y_pred = clf.predict_proba(validation_samples_scaled)
+                probas = np.array(y_pred)[:,1]
+                fpr, tpr, thresh_list = roc_curve(validation_labels, probas)
+
+                gmeans = np.sqrt(tpr * (1-fpr))
+                ix = np.argmax(gmeans)
+                thresh = thresh_list[ix]
+
+                acc = accuracy_score(validation_labels, probas>=thresh)
+                f1 = f1_score(validation_labels, probas>=thresh)
                 if f1 > best:
                     best_params = (clf.best_ntree_limit, max_depth, lr, acc, f1)
                     best = f1
                     best_model  = clf
-                    y_score = best_model.predict(validation_samples_scaled)
-                    _, _, best_thresh = roc_curve(validation_labels, y_score[:,1])
+                    best_thresh = thresh
+                   
                     
-                print(f"{clf.best_ntree_limit:^7} | {max_depth:^7} | {min_child_weight:^7} | {lr:^7} | {acc:^12} | {f1:^9}")
-            ############
-            print()
-            print(f"Best Params:")
-            print(f"{best_params[0]:^7} | {best_params[1]:^7} | {best_params[2]:^7} | {best_params[3]:^12} | {best_params[4]:^9}")
+                print(f"{best_params[0]:^7} | {best_params[1]:^7} | {best_params[2]:^7} |{best_thresh:^12} | {best_params[3]:^12} | {best_params[4]:^9}")
+    ############
+    print()
+    print(f"Best Params:")
+    print(f"{best_params[0]:^7} | {best_params[1]:^7} | {best_params[2]:^7} |{best_thresh:^12} | {best_params[3]:^12} | {best_params[4]:^9}")
+
     
     return(best_model, best_thresh)
